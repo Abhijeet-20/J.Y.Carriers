d341cbb2c5005818b78dbcaf4595f3e6f29cddf2
Abhijeet <132607217+Abhijeet-20@users.noreply.github.com>
2023-10-02 22:39:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const ham = document.querySelector('.ham');
+const ul = document.querySelector('.nav2 ul');
+
+ham.addEventListener('click', () => {
+  ul.classList.toggle('ham_active');
+  let ham_child = ham.firstElementChild;
+  if (ul.classList.contains('ham_active')) {
+    ham_child.classList.replace('ri-menu-line', 'ri-close-line');
+  }
+  else {
+    ham_child.classList.replace('ri-close-line', 'ri-menu-line');
+  }
+});
